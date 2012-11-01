a0141ba685959476bdabf8275ead62f457230a45
Faheem <faheem@gmail.com>
2012-11-01 16:42:46 +0100

    added license files and applied license headers to all files

--- netcare-auth/samlTestApplication/src/main/java/org/callistasoftware/netcare/commons/saml/testApplication/web/controller/MainController.java
+++ netcare-auth/samlTestApplication/src/main/java/org/callistasoftware/netcare/commons/saml/testApplication/web/controller/MainController.java
@@ -1,7 +1,20 @@
+/**
+ *    Copyright 2011,2012 Callista Enterprise AB
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.callistasoftware.netcare.commons.saml.testApplication.web.controller;
 
-//import org.callistasoftware.netcare.commons.auth.api.implementation.MedicalPersonalUserImpl;
-//import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
