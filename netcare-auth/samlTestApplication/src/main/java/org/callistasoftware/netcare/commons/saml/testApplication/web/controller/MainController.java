126c5e3200d76076378170b40c56004862e8d149
Marcus Krantz <krantz.marcus@gmail.com>
2012-11-01 10:48:34 +0100

    Added custom built spring security saml module and fixed some config.

--- netcare-auth/samlTestApplication/src/main/java/org/callistasoftware/netcare/commons/saml/testApplication/web/controller/MainController.java
+++ netcare-auth/samlTestApplication/src/main/java/org/callistasoftware/netcare/commons/saml/testApplication/web/controller/MainController.java
@@ -13,14 +13,6 @@ public class MainController {
 	
 	@RequestMapping("/hi")
 	public String index(ModelMap model) {
-//		MedicalPersonalUserImpl mUser =
-//				 (MedicalPersonalUserImpl)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//
-//		model.addAttribute("username", mUser.getUsername());
-//		model.addAttribute("name", mUser.getName());
-//		model.addAttribute("isDoctor", mUser.isDoctor());
-//		model.addAttribute("careUnits", mUser.getCareUnits().size());
-//		model.addAttribute("details", mUser.toString());
 		return "index";
 	}
 	
